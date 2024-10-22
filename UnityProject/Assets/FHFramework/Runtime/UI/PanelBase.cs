05a9be1de6bdd97171c25509d880787ab0444b80
DaoEer <91325804+DaoEer@users.noreply.github.com>
2024-10-22 18:58:07 +0800

    Update_UISystem

--- UnityProject/Assets/FHFramework/Runtime/UI/PanelBase.cs
+++ UnityProject/Assets/FHFramework/Runtime/UI/PanelBase.cs
@@ -2,6 +2,29 @@ namespace FHFramework
 {
     public class PanelBase
     {
+        protected virtual void OnInstantiation()
+        {
 
+        }
+
+        protected virtual void OnRefresh()
+        {
+
+        }
+
+        protected virtual void OnUpdate()
+        {
+
+        }
+
+        protected virtual void OnClose()
+        {
+
+        }
+
+        protected virtual void OnDestory()
+        {
+
+        }
     }
 }
--- UnityProject/Assets/FHFramework/Runtime/UI/UIModule.cs
+++ UnityProject/Assets/FHFramework/Runtime/UI/UIModule.cs
@@ -2,6 +2,9 @@ namespace FHFramework
 {
     public partial class UIModule : FHFrameworkModule
     {
+        public void ShowPanel<T>()
+        {
 
+        }
     }
 }
